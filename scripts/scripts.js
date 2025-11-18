12a5a604e8792a0f51a55df89a926f62d5233e48
Cole Walker <colew@ensemble.com>
2025-11-18 08:19:40 -0800

    update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -11,19 +11,31 @@ import {
   loadSection,
   loadSections,
   loadCSS,
-} from './aem.js';
+} from "./aem.js";
+
+import {
+  initMartech,
+  updateUserConsent,
+  martechEager,
+  martechLazy,
+  martechDelayed,
+} from "../plugins/martech/src/index.js";
 
 /**
  * Builds hero block and prepends to main in a new section.
  * @param {Element} main The container element
  */
 function buildHeroBlock(main) {
-  const h1 = main.querySelector('h1');
-  const picture = main.querySelector('picture');
+  const h1 = main.querySelector("h1");
+  const picture = main.querySelector("picture");
   // eslint-disable-next-line no-bitwise
-  if (h1 && picture && (h1.compareDocumentPosition(picture) & Node.DOCUMENT_POSITION_PRECEDING)) {
-    const section = document.createElement('div');
-    section.append(buildBlock('hero', { elems: [picture, h1] }));
+  if (
+    h1 &&
+    picture &&
+    h1.compareDocumentPosition(picture) & Node.DOCUMENT_POSITION_PRECEDING
+  ) {
+    const section = document.createElement("div");
+    section.append(buildBlock("hero", { elems: [picture, h1] }));
     main.prepend(section);
   }
 }
@@ -34,7 +46,8 @@ function buildHeroBlock(main) {
 async function loadFonts() {
   await loadCSS(`${window.hlx.codeBasePath}/styles/fonts.css`);
   try {
-    if (!window.location.hostname.includes('localhost')) sessionStorage.setItem('fonts-loaded', 'true');
+    if (!window.location.hostname.includes("localhost"))
+      sessionStorage.setItem("fonts-loaded", "true");
   } catch (e) {
     // do nothing
   }
@@ -50,7 +63,7 @@ function buildAutoBlocks(main) {
     const fragments = main.querySelectorAll('a[href*="/fragments/"]');
     if (fragments.length > 0) {
       // eslint-disable-next-line import/no-cycle
-      import('../blocks/fragment/fragment.js').then(({ loadFragment }) => {
+      import("../blocks/fragment/fragment.js").then(({ loadFragment }) => {
         fragments.forEach(async (fragment) => {
           try {
             const { pathname } = new URL(fragment.href);
@@ -58,7 +71,7 @@ function buildAutoBlocks(main) {
             fragment.parentElement.replaceWith(frag.firstElementChild);
           } catch (error) {
             // eslint-disable-next-line no-console
-            console.error('Fragment loading failed', error);
+            console.error("Fragment loading failed", error);
           }
         });
       });
@@ -67,7 +80,7 @@ function buildAutoBlocks(main) {
     buildHeroBlock(main);
   } catch (error) {
     // eslint-disable-next-line no-console
-    console.error('Auto Blocking failed', error);
+    console.error("Auto Blocking failed", error);
   }
 }
 
@@ -90,22 +103,50 @@ export function decorateMain(main) {
  * @param {Element} doc The container element
  */
 async function loadEager(doc) {
-  document.documentElement.lang = 'en';
+  const titaniumPromise = initMartech(
+    {
+      datastreamId: "b68d0ba1-084b-4827-aaad-d34f192e552c",
+      orgId: "FFCD7E1464AD1F2F0A495FB7@AdobeOrg",
+      debugEnabled: true,
+      onBeforeEventSend: (payload) => {},
+    },
+    {
+      alloyInstanceName: "titanium",
+    },
+  );
+
+  const initCopper = async () => {
+    window.__alloyNS ||= [];
+    window.__alloyNS.push("copper");
+    window.copper = (...args) =>
+      new Promise((resolve, reject) => {
+        window.setTimeout(() => {
+          window.copper.q.push([resolve, reject, args]);
+        });
+      });
+    window.copper.q = [];
+
+    await import("../plugins/martech/src/alloy.min.js");
+
+    return window.copper("configure", {
+      datastreamId: "dcc2912d-24b2-498d-85f4-f275d8d99969",
+      orgId: "89897F595D832D240A495FE6@AdobeOrg",
+      debugEnabled: true,
+    });
+  };
+
+  document.documentElement.lang = "en";
   decorateTemplateAndTheme();
-  const main = doc.querySelector('main');
+  const main = doc.querySelector("main");
+
   if (main) {
     decorateMain(main);
-    document.body.classList.add('appear');
-    await loadSection(main.querySelector('.section'), waitForFirstImage);
-  }
-
-  try {
-    /* if desktop (proxy for fast connection) or fonts already loaded, load fonts.css */
-    if (window.innerWidth >= 900 || sessionStorage.getItem('fonts-loaded')) {
-      loadFonts();
-    }
-  } catch (e) {
-    // do nothing
+    document.body.classList.add("appear");
+    await Promise.all([
+      titaniumPromise.then(martechEager),
+      initCopper(),
+      loadSection(main.querySelector(".section"), waitForFirstImage),
+    ]);
   }
 }
 
@@ -114,15 +155,16 @@ async function loadEager(doc) {
  * @param {Element} doc The container element
  */
 async function loadLazy(doc) {
-  const main = doc.querySelector('main');
+  const main = doc.querySelector("main");
   await loadSections(main);
 
   const { hash } = window.location;
   const element = hash ? doc.getElementById(hash.substring(1)) : false;
   if (hash && element) element.scrollIntoView();
 
-  loadHeader(doc.querySelector('header'));
-  loadFooter(doc.querySelector('footer'));
+  loadHeader(doc.querySelector("header"));
+  loadFooter(doc.querySelector("footer"));
+  await martechLazy();
 
   loadCSS(`${window.hlx.codeBasePath}/styles/lazy-styles.css`);
   loadFonts();
@@ -133,9 +175,10 @@ async function loadLazy(doc) {
  * without impacting the user experience.
  */
 function loadDelayed() {
-  // eslint-disable-next-line import/no-cycle
-  window.setTimeout(() => import('./delayed.js'), 3000);
-  // load anything that can be postponed to the latest here
+  window.setTimeout(() => {
+    martechDelayed();
+    import("./delayed.js");
+  }, 3000);
 }
 
 async function loadPage() {
